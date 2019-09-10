a5f6659063288a364bbd9f0ae6c1778d9d9e6ad2
Mark Ryan <markryandeveloper@gmail.com>
2019-09-10 18:05:28 -0400

    modified dummy content data

--- dummy_data.py
+++ dummy_data.py
@@ -21,15 +21,15 @@ def dummy_data():
                 models.Entry.create_entry(
                     author=models.User.get_by_id(1),
                     title="Test Entry {}".format(i),
-                    content="Lorem ipsum dolor sit amet, consectetur"
-                             "adipiscing elit, sed do eiusmod tempor incididunt ut"
-                             "labore et dolore magna aliqua. Ut enim ad minim veniam,"
-                             "quis nostrud exercitation ullamco laboris nisi ut"
-                             "aliquip ex ea commodo consequat. Duis aute irure dolor"
-                             "in reprehenderit in voluptate velit esse cillum dolore"
-                             "eu fugiat nulla pariatur. Excepteur sint occaecat"
-                             "cupidatat non proident, sunt in culpa qui officia"
-                             "deserunt mollit anim id est laborum.",
+                    content="Lorem ipsum dolor sit amet, consectetur "
+                    "adipiscing elit, sed do eiusmod tempor incididunt ut "
+                    "labore et dolore magna aliqua. Ut enim ad minim veniam, "
+                    "quis nostrud exercitation ullamco laboris nisi ut "
+                    "aliquip ex ea commodo consequat. Duis aute irure dolor "
+                    "in reprehenderit in voluptate velit esse cillum dolore "
+                    "eu fugiat nulla pariatur. Excepteur sint occaecat "
+                    "cupidatat non proident, sunt in culpa qui officia "
+                    "deserunt mollit anim id est laborum. ",
                     date=datetime.datetime.now(),
                     time_spent=i,
                 )
